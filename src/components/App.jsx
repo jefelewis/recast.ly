238a922072798c2c4b6d32b3cd4909456c0bcf4e
Hack Reactor Student <student@hackreactor.com>
2018-01-31 15:44:29 -0800

    Search bar/Video player working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,15 @@ var App = () => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        <Search />
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <VideoPlayer />
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <VideoList exampleVideo = {exampleVideoData} />
       </div>
     </div>
   </div>
@@ -19,4 +19,4 @@ var App = () => (
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 ReactDOM.render(<App />, document.getElementById('app'));
-//window.App = App;
+window.App = App;
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,11 +1,9 @@
-var VideoList = () => (
+var VideoList = (props) => (
+  
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <VideoListEntry video = {this.props.VideoListEntry[0]}/>
   </div>
+
 );
 
 // PropTypes tell other developers what `props` a component expects
